015405730a06a17bdfc069b3c7eca331e1f8b377
YusufSengoz <135019752+zekirovskii@users.noreply.github.com>
2025-09-13 15:43:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: "/",
     build: {
       sourcemap: true,
       rollupOptions: {
